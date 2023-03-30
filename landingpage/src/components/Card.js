3ec8035e9aa383c99376fcd7813e76d1f0466b2a
matiascrovetto <matiastapiacrovetto@gmail.com>
2023-03-30 02:41:42 +0000

    landingp

--- landingpage/src/components/Card.js
+++ landingpage/src/components/Card.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom/client';
 
 function Card(props) {
     return (
-        <div class="m-5 bg-success col-12 text-black">
-            <img class="w-100 h-50" src={props.cardImageUrl} alt="Card image" />
-            <div class="card-body">
-                <h5 class="card-title">{props.cardTitle}</h5>
-                <p class="card-text">{props.cardDescription} </p>
-                <a href={props.cardLink} class="btn btn-light">{props.cardButton}</a>
+        <div className="m-5 bg-success col-12 text-black">
+            <img className="w-100 h-50" src={props.cardImageUrl} alt="Card image" />
+            <div className="card-body">
+                <h5 className="card-title">{props.cardTitle}</h5>
+                <p className="card-text">{props.cardDescription} </p>
+                <a href={props.cardLink} className="btn btn-light">{props.cardButton}</a>
             </div>
         </div>
     )
--- landingpage/src/components/Jumbo.js
+++ landingpage/src/components/Jumbo.js
@@ -1,14 +1,14 @@
 function Jumbo() {
     return (
-      <div class="container py-4">
-        <header class="pb-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center text-dark text-decoration-none"> </a>
+      <div className="container py-4">
+        <header className="pb-3 mb-4 border-bottom">
+          <a href="/" className="d-flex align-items-center text-dark text-decoration-none"> </a>
         </header>
-        <div class="p-5 mb-4 bg-success rounded-3">
-          <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">¡Choose your favorite pet!</h1>
-            <p class="col-md-8 fs-4">Pets provide many benefits to humans. They comfort us and they give us companionship. However, some animals can also pass diseases to people.</p>
-            <button class="btn btn-light btn-lg" type="button">Find your best friend here!</button>
+        <div className="p-5 mb-4 bg-success rounded-3">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">¡Choose your favorite pet!</h1>
+            <p className="col-md-8 fs-4">Pets provide many benefits to humans. They comfort us and they give us companionship. However, some animals can also pass diseases to people.</p>
+            <button className="btn btn-light btn-lg" type="button">Find your best friend here!</button>
           </div>
         </div>
       </div>
